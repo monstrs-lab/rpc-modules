a85ec9e33fa2471835a976e380d329ded807df08
Andrey Linko <AndreyLinko@gmail.com>
2023-06-18 22:58:01 +0300

    feat: json transformers

--- packages/rpc-transformers/src/index.ts
+++ packages/rpc-transformers/src/index.ts
@@ -1,2 +1,4 @@
 export * from './date-to-timestamp.transformer.js'
 export * from './timestamp-to-date.transformer.js'
+export * from './json-to-string.transformer.js'
+export * from './string-to-json.transformer.js'
