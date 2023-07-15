2fdf94cbe8667a05096ef44d81cbc3fbed60a537
Andrey Linko <AndreyLinko@gmail.com>
2023-07-16 00:09:47 +0300

    feat: date to timestamp trasform methods

--- packages/rpc-transformers/src/timestamp-to-date.transformer.ts
+++ packages/rpc-transformers/src/timestamp-to-date.transformer.ts
@@ -1,11 +1,6 @@
-import { Timestamp } from '@bufbuild/protobuf'
-import { Transform } from 'class-transformer'
+import type { Timestamp } from '@bufbuild/protobuf'
+
+import { Transform }      from 'class-transformer'
 
 export const TimestampToDateTransform = (): PropertyDecorator =>
-  Transform(({ value }: { value: Timestamp }): Date | undefined => {
-    if (value instanceof Timestamp) {
-      return value.toDate()
-    }
-
-    return undefined
-  })
+  Transform(({ value }: { value: Timestamp }): Date | undefined => value?.toDate?.())
