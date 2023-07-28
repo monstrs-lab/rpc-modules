d5f2a2bf00a4d1217ccda56f86046ecc9f5fd714
Andrey Linko <AndreyLinko@gmail.com>
2023-07-28 21:38:05 +0300

    feat: query payload optional values

--- packages/rpc-query-payloads/src/queries/id-query.payload.ts
+++ packages/rpc-query-payloads/src/queries/id-query.payload.ts
@@ -6,17 +6,17 @@ import { ValidateNested }      from 'class-validator'
 import { IdConditionsPayload } from '../conditions/index.js'
 
 export class IdQueryPayload implements Query.IDType {
-  constructor(private readonly query: Query.IDType) {}
+  constructor(private readonly query?: Query.IDType) {}
 
   @IsOptional()
   @IsEnum(Query.Operator)
   get operator(): Query.Operator.AND | Query.Operator.OR | 0 | 1 | undefined {
-    return this.query.operator
+    return this.query?.operator
   }
 
   @IsOptional()
   @ValidateNested()
   get conditions(): IdConditionsPayload | undefined {
-    return new IdConditionsPayload(this.query.conditions)
+    return new IdConditionsPayload(this.query?.conditions)
   }
 }
