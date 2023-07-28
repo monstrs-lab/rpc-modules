8742f7a405bb5978a63a139ce58f37040b0ebae9
Andrey Linko <AndreyLinko@gmail.com>
2023-07-28 21:45:15 +0300

    feat: query payload search, order and pager

--- packages/rpc-query-payloads/src/search/search-field.payload.ts
+++ packages/rpc-query-payloads/src/search/search-field.payload.ts
@@ -3,6 +3,10 @@ import type { Query } from '@monstrs/query-types'
 import { IsString }   from 'class-validator'
 
 export class SearchFieldPayload implements Query.SearchField {
+  constructor(private readonly field: Query.SearchField) {}
+
   @IsString({ each: true })
-  path!: Array<string>
+  get path(): Array<string> {
+    return this.field.path
+  }
 }
